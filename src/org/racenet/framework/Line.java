a66d1d302a160bbc7bd602749f688776f2f3becc
Andreas Linden <zlx@gmx.de>
2012-03-28 15:24:46 +0200

    added testing against NaN and infinity values to line intersection

--- src/org/racenet/framework/Line.java
+++ src/org/racenet/framework/Line.java
@@ -1,5 +1,7 @@
 package org.racenet.framework;
 
+import android.util.Log;
+
 public class Line {
 
 	public Vector2 p1;
@@ -33,7 +35,18 @@ public class Line {
 					((this.p1.x - this.p2.x) * (other.p1.y - other.p2.y) - (this.p1.y - this.p2.y) *
 					(other.p1.x - other.p2.x));
 		
-		if ((px < l1l || px > l1r || px < l2l || px > l2r || py < l1b || py > l1t || py < l2b || py > l2t)) {
+		Log.d("INTERSECT", "x " + String.valueOf(new Float(px)) + " y " + String.valueOf(new Float(py)));
+		
+		Float fpx = new Float(px);
+		Float fpy = new Float(py);
+		
+		if (fpx.compareTo(Float.NaN) == 0 || fpx.compareTo(Float.NEGATIVE_INFINITY) == 0 || fpx.compareTo(Float.POSITIVE_INFINITY) == 0 ||
+			fpy.compareTo(Float.NaN) == 0 || fpy.compareTo(Float.NEGATIVE_INFINITY) == 0 || fpy.compareTo(Float.POSITIVE_INFINITY) == 0) {
+			
+			return false;
+		}
+		
+		if (px < l1l || px > l1r || px < l2l || px > l2r || py < l1b || py > l1t || py < l2b || py > l2t) {
 			
 			return false;
 			

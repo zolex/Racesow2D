02bc64828fd91d2b0ee6d77e007cb4bfc3c0737b
Andreas Linden <zlx@gmx.de>
2012-04-03 12:11:16 +0200

    made cameratext for flexible

--- src/org/racenet/framework/CameraText.java
+++ src/org/racenet/framework/CameraText.java
@@ -8,6 +8,12 @@ public class CameraText extends HudItem {
 	GL10 gl;
 	BitmapFont font;
 	public String text = "empty";
+	public float red = 0;
+	public float green = 1;
+	public float blue = 0;
+	public float alpha = 1;
+	public float scale = 0.075f;
+	public float space = 0.06f;
 	
 	public CameraText(SpriteBatcher batcher, BitmapFont font, GL10 gl, float cameraX, float cameraY) {
 		
@@ -20,8 +26,8 @@ public class CameraText extends HudItem {
 	@Override
 	public void draw() {
 		
-		this.gl.glColor4f(0, 1, 0, 1);
-		this.font.draw(batcher, this.text, 0.075f, 0.06f, this.getPosition().x, this.getPosition().y);
+		this.gl.glColor4f(this.red, this.green, this.blue, this.alpha);
+		this.font.draw(batcher, this.text, this.scale, this.space, this.getPosition().x, this.getPosition().y);
 		this.gl.glColor4f(1, 1, 1, 1);
 	}
 

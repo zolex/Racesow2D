54de79f841ae41d526a975a8897886542569bede
Andreas Linden <zlx@gmx.de>
2012-04-03 12:30:33 +0200

    fixed warnings in submitscoresthread

--- src/org/racenet/racesow/threads/SubmitScoreThread.java
+++ src/org/racenet/racesow/threads/SubmitScoreThread.java
@@ -9,31 +9,23 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.racenet.framework.Downloader;
-import org.racenet.helpers.InputStreamToString;
-
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 
 public class SubmitScoreThread extends Thread {
 
 	private float time;
 	private String map;
 	private String player;
-	private short tries;
+	private short submitTries;
 	
 	public SubmitScoreThread(String map, String player, float time) {
 		
 		this.time = time;
 		this.map = map;
 		this.player = player;
-		this.tries = 0;
+		this.submitTries = 0;
 	}
 	
 	@Override
@@ -55,15 +47,15 @@ public class SubmitScoreThread extends Thread {
 	        nameValuePairs.add(new BasicNameValuePair("time", String.valueOf(new Float(this.time))));
 	        nameValuePairs.add(new BasicNameValuePair("key", "alpha-key"));
 	        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-	        HttpResponse response = client.execute(post);
+	        client.execute(post);
 	        
 	    } catch (ClientProtocolException e) {
 	    	
-	    	if (this.tries++ < 10) this.submitScores();
+	    	if (this.submitTries++ < 10) this.submitScores();
 	    	
 	    } catch (IOException e) {
     		
-	    	if (this.tries++ < 10) this.submitScores();
+	    	if (this.submitTries++ < 10) this.submitScores();
 	    }
 	}
 }

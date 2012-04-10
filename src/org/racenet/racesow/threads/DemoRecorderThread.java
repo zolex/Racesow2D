9a84380d8ff26d3ba6adce37efa533593e9cc1d5
Andreas Linden <zlx@gmx.de>
2012-04-10 20:28:04 +0200

    fixed warning

--- src/org/racenet/racesow/threads/DemoRecorderThread.java
+++ src/org/racenet/racesow/threads/DemoRecorderThread.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;

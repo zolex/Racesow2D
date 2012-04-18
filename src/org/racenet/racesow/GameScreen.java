478f6fa53e943c9b20a6ea22eba996f5cc2f0d2f
Andreas Linden <zlx@gmx.de>
2012-04-19 00:29:01 +0200

    fixed warning

--- src/org/racenet/racesow/GameScreen.java
+++ src/org/racenet/racesow/GameScreen.java
@@ -21,7 +21,6 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.DialogInterface.OnClickListener;
 import android.opengl.GLES10;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;

ff607f36d1435f1d36716a66a7d56d5671fd0053
Andreas Linden <zlx@gmx.de>
2012-04-24 21:41:55 +0200

    made XMLLoaderTask capable of get and post requests

--- src/org/racenet/racesow/threads/XMLLoaderTask.java
+++ src/org/racenet/racesow/threads/XMLLoaderTask.java
@@ -2,14 +2,18 @@ package org.racenet.racesow.threads;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import org.racenet.racesow.XMLListActivity;
+import org.racenet.racesow.XMLCallback;
 
 import android.os.AsyncTask;
 
@@ -19,20 +23,71 @@ import android.os.AsyncTask;
  * @author soh#zolex
  *
  */
-public class XMLLoaderTask extends AsyncTask<String, Void, InputStream> {
+public class XMLLoaderTask extends AsyncTask<Object, Void, InputStream> {
 
-	XMLListActivity activity;
+	XMLCallback activity;
 	
-	public XMLLoaderTask(XMLListActivity activity) {
+	public XMLLoaderTask(XMLCallback activity) {
 		
 		this.activity = activity;
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
-	protected InputStream doInBackground(String... urls) {
+	protected InputStream doInBackground(Object... input) {
+		
+		if (input.length == 1) {
+			
+			return this.getRequest((String)input[0]);
+		
+		} else if (input.length == 2) {
+			
+			return this.postRequest((String)input[0], (List<NameValuePair>)input[1]);
+			
+		} else {
+			
+			return null;
+		}
+	}
+	
+	/**
+	 * Execute a get request and return the response
+	 * 
+	 * @param String url
+	 * @param List<NameValuePair> values
+	 * @return InputStream
+	 */
+	public InputStream postRequest(String url, List<NameValuePair> values) {
 		
 		HttpClient client = new DefaultHttpClient();				
-		HttpGet get = new HttpGet(urls[0]);
+		HttpPost post = new HttpPost(url);
+		
+	    try {
+	    	
+	    	post.setEntity(new UrlEncodedFormEntity(values));
+	        HttpResponse response = client.execute(post);
+	        return response.getEntity().getContent(); 
+	        
+	    } catch (ClientProtocolException e) {
+	    	
+	    	return null;
+
+	    } catch (IOException e) {
+
+	    	return null;
+	    }
+	}
+	
+	/**
+	 * Execute a get request and return the response
+	 * 
+	 * @param String url
+	 * @return InputStream
+	 */
+	public InputStream getRequest(String url) {
+		
+		HttpClient client = new DefaultHttpClient();				
+		HttpGet get = new HttpGet(url);
 	    
 	    try {
 	    	
